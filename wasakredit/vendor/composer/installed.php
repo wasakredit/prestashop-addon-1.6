c7a5570ae8d98b1da32a57ef5bcb41799f8996de
KFoobar <david@kfoobar.se>
2022-05-30 20:54:12 +0200

    Add support for Invoice Checkout. Refactor codebase.

---
+++ wasakredit/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '509ff293d0784a7119f3aef71bc75e381f48a06b',
+        'name' => 'wasa/prestashop-module',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wasa/client-php-sdk' => array(
+            'pretty_version' => 'v3.0',
+            'version' => '3.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wasa/client-php-sdk',
+            'aliases' => array(),
+            'reference' => '5e36a430694e7f1d3d73c72968ee5d960744b306',
+            'dev_requirement' => false,
+        ),
+        'wasa/prestashop-module' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '509ff293d0784a7119f3aef71bc75e381f48a06b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
